faf62efb366857f92390ce2f629c4a222bcf68b1
LAYAGODFREYCLEMENT <layagodfrey12@gmail.com>
2023-01-10 01:28:28 +0300

    created the ProductCatergory class in models file

--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.translation import gettext_lazy
+from django.contrib.postgres import fields as PostgresFields
 
 # Create your models here.
+class ProductCatergory(models.models):
+    name = models.CharField(max_length=256)
+    icon_url = models.URLField(blank=True)
+    description = models.TextField(blank=False)
+    parent_catergory = models.ForeignKey(
+        "self",
+        blank=True,
+        null=True,
+        related_name="children_catergories",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
