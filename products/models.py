faadfde0d92bb0a8c5469378988976690976fdb6
LAYAGODFREYCLEMENT <layagodfrey12@gmail.com>
2023-01-10 03:13:14 +0300

    created the product class

--- products/models.py
+++ products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.translation import gettext_lazy
+from django.utils.translation import gettext_lazy as _
 from django.contrib.postgres import fields as PostgresFields
 
 # Create your models here.
@@ -24,3 +24,36 @@ class Maker(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    class Currency(models.TextChoices):
+        SWEDISH_CROWN = ("SEK", _("Swedish Crown"))
+        AMERICAN_DOLLAR = ("USD", _("American Dollar"))
+        EURO = ("EUR", _("Euro"))
+        POUND_STERLING = ("GBP", _("Pound Sterling"))
+        YEN = ("JPY", _("Yen"))
+        AUSTRIALIAN_DOLLAR = ("AUD", _("Austrian Dollar"))
+        UGANDAN_SHILLINGS = ("Ugx", _("Ugandan Shillings"))
+        KENYAN_SHILLINGS = ("Kys", _("Kenyan Shillings"))
+
+    title = models.CharField(max_length=512)
+    subtitle = models.CharField(max_length=512)
+    maker = models.ForeignKey(Maker, on_delete=models.CASCADE, blank=True, null=True)
+
+    image1_url = models.URLField(blank=True, null=True)
+    image2_url = models.URLField(blank=True, null=True)
+    image3_url = models.URLField(blank=True, null=True)
+    image4_url = models.URLField(blank=True, null=True)
+
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    currency = models.CharField(
+        max_length=3, choices=Currency.choices, default=Currency.AMERICAN_DOLLAR
+    )
+
+    variantion_product_ids = PostgresFields.ArrayField(
+        models.IntegerField(null=True, blank=True), null=True, blank=True
+    )
+
+    def __str__(self):
+        return f"{self.title}-{self.subtitle} -{self.naker}"
