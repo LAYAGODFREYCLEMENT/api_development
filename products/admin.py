f2522df21468ab087db7fdf8c5994c5baa5800a7
LAYAGODFREYCLEMENT <layagodfrey12@gmail.com>
2023-01-10 03:20:46 +0300

    register ProductCatergory ,Maker and Product models to admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ProductCatergory, Maker, Product
 
 # Register your models here.
+admin.site.register(ProductCatergory)
+admin.site.register(Maker)
+admin.site.register(Product)
